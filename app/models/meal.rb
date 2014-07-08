c5bf046b8335e358484c5133c942ea10e81e6087
shirui <shiruide@gmail.com>
2014-07-09 01:03:34 +0800

    enhance sort for meal

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,6 +3,8 @@ require 'acts_as_list'
 class Meal < ActiveRecord::Base
   acts_as_list
   
+  default_scope { order 'position' }
+  
   MEAL_TYPES = ['breakfast', "lunch", "dinner"]
   
   def name
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :dish
   belongs_to :meal
+  
+  default_scope { order 'meal_id' }
+  
 end
