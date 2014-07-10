1b7835c3568cda8f538f250a7d50df6f82dc7d93
shirui <shiruide@gmail.com>
2014-07-11 00:20:34 +0800

    simplify nested show for meal and dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,11 +2,9 @@ class Dish < ActiveRecord::Base
   belongs_to :book
   
   has_many :burdens, :dependent => :destroy
-  accepts_nested_attributes_for :burdens
-  has_and_belongs_to_many :materials, join_table: :burdens 
-  
   has_many :orders, :dependent => :destroy
-  has_and_belongs_to_many :meals, join_table: :orders 
+  
+  accepts_nested_attributes_for :burdens
   
   default_scope { order 'name' }
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,7 +3,6 @@ require 'acts_as_list'
 class Meal < ActiveRecord::Base
   acts_as_list
   has_many :orders, :dependent => :destroy
-  has_and_belongs_to_many :dishes, join_table: :orders
   
   default_scope { order 'position' }
   
