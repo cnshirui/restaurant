bbc6f51f337df8ddfbcd692cd688fd614ed6ee5c
Rui Shi <shiruide@gmail.com>
2014-07-10 18:03:06 +0800

    1. add material for dish.show 2. more translate

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,11 @@
 class Dish < ActiveRecord::Base
   belongs_to :book
-  has_and_belongs_to_many :dishes
+  
+  has_many :burdens
+  has_and_belongs_to_many :materials, join_table: :burdens 
+  
+  has_many :orders
+  has_and_belongs_to_many :meals, join_table: :orders 
   
   default_scope { order 'name' }
 end
