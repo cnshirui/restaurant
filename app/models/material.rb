ffa82d0e60eb999dc30b228f80346a4ff7b85660
Rui Shi <shiruide@gmail.com>
2014-07-14 16:06:22 +0800

    before fix mapping issue

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,5 @@
 class Material < ActiveRecord::Base
+  
+  has_and_belongs_to_many :meal, join_table: :burdens
+  
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -4,6 +4,9 @@ class Meal < ActiveRecord::Base
   acts_as_list
   has_many :orders, :dependent => :destroy
   
+  has_many :burdens
+  has_and_belongs_to_many :material, join_table: :burdens
+  
   default_scope { order 'position' }
   
   MEAL_TYPES = ['breakfast', "lunch", "dinner"]
